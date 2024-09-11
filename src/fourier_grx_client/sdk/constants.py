0cf39b1b1120bcb49ddd5acdbc0c68b70731a10b
Jason Chen <chenxin199305@163.com>
2024-09-11 11:06:52 +0800

    update code to catchup the latest code in fourier-grx sdk layer, make them to be the same code.

--- src/fourier_grx_client/sdk/constants.py
+++ src/fourier_grx_client/sdk/constants.py
@@ -1,14 +1,16 @@
-import numpy as np
+import numpy
+
+BASE_LINK = "base"
 
 # fmt: off
-DEFAULT_POSITIONS = np.rad2deg(np.array(
+DEFAULT_POSITIONS = numpy.rad2deg(numpy.array(
     [
-        0.0, 0.0, -0.2618, 0.5236, -0.2618, 0.0,  # left leg (6)
-        0.0, 0.0, -0.2618, 0.5236, -0.2618, 0.0,  # right leg (6)
-        0.0, 0.0, 0.0,  # waist (3)
-        0.0, 0.0, 0.0,  # head (3)
-        0.0, 0.2, 0.0, -0.3, 0.0, 0.0, 0.0,  # left arm (7)
-        0.0, -0.2, 0.0, -0.3, 0.0, 0.0, 0.0,  # right arm (7)
+        0.0, 0.0, -0.2618, 0.5236, -0.2618, 0.0,  # left leg
+        0.0, 0.0, -0.2618, 0.5236, -0.2618, 0.0,  # right leg
+        0.0, 0.0, 0.0,  # waist
+        0.0, 0.0, 0.0,  # head
+        0.0, 0.2, 0.0, -0.3, 0.0, 0.0, 0.0,  # left arm
+        0.0, -0.2, 0.0, -0.3, 0.0, 0.0, 0.0,  # right arm
     ]
 ))
 # fmt: on
--- src/fourier_grx_client/sdk/utils.py
+++ src/fourier_grx_client/sdk/utils.py
@@ -1,12 +1,17 @@
+import numpy
+import msgpack
+import msgpack_numpy
+
 from dataclasses import dataclass
 from enum import Enum, IntEnum
 from typing import Any
 
-import msgpack
-import msgpack_numpy as m
-import numpy as np
+msgpack_numpy.patch()
 
-m.patch()
+
+class FunctionResult(IntEnum):
+    SUCCESS = 0
+    FAIL = -1
 
 
 class ControlMode(IntEnum):
@@ -17,6 +22,25 @@ class ControlMode(IntEnum):
     POSITION = 0x04
     PD = 0x09
     OTHER = 0x0A
+
+
+class ControlConfig:
+    control_mode: ControlMode
+    position_control_kp = None
+    velocity_control_kp = None
+    velocity_control_ki = None
+    pd_control_kp = None
+    pd_control_kd = None
+
+    def to_dict(self):
+        return {
+            "control_mode": self.control_mode,
+            "position_control_kp": self.position_control_kp,
+            "velocity_control_kp": self.velocity_control_kp,
+            "velocity_control_ki": self.velocity_control_ki,
+            "pd_control_kp": self.pd_control_kp,
+            "pd_control_kd": self.pd_control_kd,
+        }
 
 
 class ControlGroup(tuple, Enum):
@@ -57,8 +81,8 @@ class ServiceStatus(str, Enum):
 
 @dataclass
 class Trajectory:
-    start: np.ndarray
-    end: np.ndarray
+    start: numpy.ndarray
+    end: numpy.ndarray
     duration: float
 
     def at(self, t: float):
